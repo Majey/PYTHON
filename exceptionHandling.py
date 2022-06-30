03b66e0aad7174d8c1b88e6208935b9fb95a9f03
Majey <kevinerick78@yahoo.com>
2022-06-30 22:38:01 +0300

    Math functions

--- exceptionHandling.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        number = int(input("Please enter a number: "))
-        break # the break forces the program terminate when valid input is entered
-
-    except ValueError:
-        print("Please enter an integer")
-
-print(f"Congratulations! { number } is an integer")
