77a29ec1bbc05a40c77c3b7a5535c9caab1ced06
Majey <kevinerick78@yahoo.com>
2022-06-30 21:35:00 +0300

    Math String Exception handling

--- python.py
+++ python.py
@@ -1,4 +1,9 @@
-while true:
+while True:
     try:
         number = int(input("Please enter a number: "))
-        
+        break # the break forces the program terminate when valid input is entered
+
+    except ValueError:
+        print("Please enter an integer")
+
+print(f"Congratulations! { number } is an integer")
